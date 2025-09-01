069fa1d1b65fe152b56079b43b59c4b29eb606c6
Yann L <yannlacaze31@gmail.com>
2025-09-01 22:20:52 +0200

    change hotkey git file

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,5 +3,5 @@ require("telescope").setup({})
 -- Add keymaps here
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
